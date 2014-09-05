f41a13e34d2068337fbe730525ba4b35a2b24059
Michael Asnes <mike.asnes@gmail.com>
2014-09-05 15:11:49 -0600

    rshift.sh added daybrightness variable
    can now set day brightness by adding number to $2

--- common/rshift.sh
+++ common/rshift.sh
@@ -3,12 +3,17 @@
 numargs=$#
 args=("$@")
 
+daybrightness=1.0
 nightbrightness=0.4
 
 if [[ $1 =~ ^[0-1]\.[0-9]+$ ]]; then
    nightbrightness=$1
 fi
 
+if [[ $2 =~ ^[0-1]\.[0-9]+$ ]]; then
+   daybrightness=$2
+fi
+
 
 pkill -x redshift
-redshift -l 40\.0\:-105\.3 -t 5700\:3300 -b 1.0:$nightbrightness &
+redshift -l 40\.0\:-105\.3 -t 5700\:3300 -b $daybrightness:$nightbrightness &
