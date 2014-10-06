ec4132b421e0a9b8889b86f9a5649c605536888b
Michael Asnes <mike.asnes@gmail.com>
2014-10-05 22:34:53 -0600

    monitor.sh new bg picture used

--- monitor.sh
+++ monitor.sh
@@ -5,9 +5,9 @@ EXT="VGA1"
 
 if [ $(xrandr | grep -c "$EXT connected") ] ; then
   xrandr --output $EXT --auto --right-of $IN --primary
-  feh --bg-scale ~/pictures/yosemite_california.jpg
+  feh --bg-scale ~/pictures/green_arch.jpg
 else
   xrandr --output $EXT --off --right-of $IN --primary
   xrandr --output $IN ---primary
-  feh --bg-scale ~/pictures/yosemite_california.jpg
+  feh --bg-scale ~/pictures/green_arch.jpg
 fi
