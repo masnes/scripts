4df6281ce18454153355cf7bf5fe0c2dee607479
Michael Asnes <mike.asnes@gmail.com>
2015-03-31 01:48:06 -0600

    added dection for HDMI1 port
    also fixed a bug where the first if statement always evaluated to true
    
    also fixed a bug where there was an extra dash before --primary in
    final if statement chain
    
    git add patch was being uncoperative, so here's all three at once

--- common/monitor.sh
+++ common/monitor.sh
@@ -3,26 +3,52 @@
 
 IN="LVDS1"
 EXT="VGA1"
+EXT2="HDMI1"
 
-if [ $(xrandr | grep -c "$EXT connected") ] ; then
+# bspwm automatically adds a new desktop, Desktop#, each time
+# a monitor is recognized by it (that way there's a desktop for
+# that monitor no matter what). However, we're re-shuffling
+# desktops, and the name Desktop# is ugly
+function remove_autogened_desktops {
+   for desktop_name in $(bspc query -D | grep Desktop); do
+      bspc desktop $desktop_name -r
+   done
+}
+
+function setup_new_monitor {
+   output_name=$1
    # add vga monitor
-   xrandr --output $EXT --auto --right-of $IN --primary
+   xrandr --output $output_name --auto --right-of $IN --primary
    # add background for new monitor
    feh --bg-scale ~/pictures/yosemite_california.jpg
    # move desktops to new monitor
-   bspc desktop V -m VGA1; \
-      bspc desktop VI -m VGA1; \
-      bspc desktop VII -m VGA1; \
-      bspc desktop VIII -m VGA1; \
-      bspc desktop IX -m VGA1
+   bspc desktop V -m $output_name; \
+      bspc desktop VI -m $output_name; \
+      bspc desktop VII -m $output_name; \
+      bspc desktop VIII -m $output_name; \
+      bspc desktop IX -m $output_name
+}
+
+if [ $(xrandr | grep -c "$EXT connected") -gt 0 ] ; then
+   echo "$EXT connected"
+   echo $(xrandr | grep -c "$EXT connected")
+   setup_new_monitor $EXT
    # remove default "Desktop#" workspace(s)
-   bspc desktop Desktop2 -r
-   bspc desktop Desktop3 -r
-   bspc desktop Desktop4 -r
+   remove_autogened_desktops
+   # restart panel
+   pkill -USR1 -x sxhkd; pkill -USR1 -x panel; $HOME/.scripts/panel/panel &
+elif [ $(xrandr | grep -c "$EXT2 connected") -gt 0 ]; then
+   echo "$EXT2 found"
+   setup_new_monitor $EXT2
+   # remove default "Desktop#" workspace(s)
+   remove_autogened_desktops
    # restart panel
    pkill -USR1 -x sxhkd; pkill -USR1 -x panel; $HOME/.scripts/panel/panel &
 else
+   echo "no external monitor found"
    xrandr --output $EXT --off --right-of $IN --primary
-   xrandr --output $IN ---primary
+   xrandr --output $IN --primary
    feh --bg-scale ~/pictures/yosemite_california.jpg
 fi
+
+
