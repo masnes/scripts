686e262f78a9e2b10e313cd4968d2babb4a87e84
Michael Asnes <mike.asnes@gmail.com>
2015-11-11 23:30:35 -0700

    fancy new options

--- common/rshift.sh
+++ common/rshift.sh
@@ -3,11 +3,52 @@
 numargs=$#
 args=("$@")
 
+
+getopt --test > /dev/null
+if [[ $? != 4 ]]; then
+  echo "I’m sorry, `getopt --test` failed in this environment."
+  exit 1
+fi
+
 daybrightness=1.0
-nightbrightness=0.4
+nightbrightness=0.9
 
 daytemp=5700
 nighttemp=2200
+
+while true; do
+  case "$1" in
+    -B|--day-brightness)
+      daybrightness=$2
+      shift
+      ;;
+    -b|--night-brightness)
+      nightbrightness=$2
+      shift
+      ;;
+    -T|--day-temp)
+      daytemp=$2
+      echo $daytemp
+      shift
+      ;;
+    -t|--night-temp)
+      nighttemp=$2
+      shift
+      ;;
+    -h|--help)
+      echo "Usage: (-B|--day-brightness) (-b|--night-brightness) (-T|--day-temp) (-t|--night-temp)"
+      exit 0
+      ;;
+    '')
+      break
+      ;;
+    *)
+      echo "option not recognized: '${1}'";
+      exit 1
+      ;;
+  esac
+  shift
+done
 
 if [[ $1 =~ ^[0-1]\.[0-9]+$ ]]; then
    nightbrightness=$1
@@ -20,7 +61,9 @@ fi
 
 pkill -x redshift
 if [[ numargs -gt 0 ]]; then
-   redshift -l 40\.0\:-105\.3 -t $daytemp\:$nighttemp -b $daybrightness:$nightbrightness &
+  set -x
+  redshift -l 40\.0\:-105\.3 -t $daytemp\:$nighttemp -b $daybrightness:$nightbrightness &
 else
+  set -x
    redshift -l 40\.0\:-105\.3 -t $daytemp\:$nighttemp &
 fi
