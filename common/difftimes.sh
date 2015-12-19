aa43222dc060b7876168ff38d6364f9c11691b25
Michael Asnes <mike.asnes@gmail.com>
2015-12-18 19:39:39 -0700

    difftimes: fix add hour/second/minute reading

--- common/difftimes.sh
+++ common/difftimes.sh
@@ -12,12 +12,19 @@ addTime() {
     else
         time="now"
     fi
+    numFields=$(echo $1 | sed -s 's/[^:]//g' | wc -c | bc)
     hours=`echo $1 | cut -d ':' -f 1`
     echo hours $hours
-    minutes=`echo $1 | cut -d ':' -f 2`
-    echo minutes $minutes
-    seconds=`echo $1 | cut -d ':' -f 3`
-    echo seconds $seconds
+    minutes=0
+    seconds=0
+    if [[ $numFields -gt 1 ]]; then
+        minutes=`echo $1 | cut -d ':' -f 2`
+        echo minutes $minutes
+    fi
+    if [[ $numFields -gt 2 ]]; then
+        seconds=`echo $1 | cut -d ':' -f 3`
+        echo seconds $seconds
+    fi
     date -d "$time + $hours hours $minutes minutes $seconds seconds" $3
 }
 
