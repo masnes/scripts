1270635c85d7b3e41a831da6cc7d5c91d24c247c
Michael Asnes <mike.asnes@gmail.com>
2015-12-18 22:32:29 -0700

    difftimes add/subtract handle numbers >24/60

--- common/difftimes.sh
+++ common/difftimes.sh
@@ -3,49 +3,65 @@
 usage() {
     scriptname=`echo $0 | xargs basename`
     echo "Usage: $scriptname diff 8:00pm [7:00pm]"
-    echo "Usage: $scriptname add 3:00 [7:05pm] [+%H:%M]"
+    echo "Usage: $scriptname add 3:00 [7:05]"
+    echo "Usage: $scriptname sub 3:00 [7:05:13]"
+}
+
+
+# given a number in the format \d\d:\d\d:\d\d
+# splite int hours, minutes, seconds
+# echo $hours $minutes $seconds
+_hours_minutes_seconds() {
+    numFields=$(echo $1 | sed -s 's/[^:]//g' | wc -c | bc)
+    if [[ $numFields -eq 0 ]]; then
+        usage 1>&2
+        exit
+    fi
+    hours=`echo $1   | cut -d ':' -f 1`
+    if [[ $numFields -gt 1 ]]; then
+        minutes=`echo $1 | cut -d ':' -f 3`
+    else
+        minutes=0
+    fi
+    if [[ $numFields -gt 2 ]]; then
+        seconds=`echo $1 | cut -d ':' -f 2`
+    else
+        seconds=0
+    fi
+    echo $hours $minutes $seconds
+}
+
+_time_add_subtract() {
+    if [[ $# -gt 2 ]]; then
+        new_time=$1
+        base_time=$2
+        operator=$3
+    elif [[ $# -gt 1 ]]; then
+        new_time=$1
+        base_time=`date "-d" "now" +%H:%M:%S`
+        operator=$2
+    else
+        usage
+        exit
+    fi
+    read base_hours base_minutes base_seconds <<<$(_hours_minutes_seconds $base_time)
+    read new_hours new_minutes new_seconds <<<$(_hours_minutes_seconds $new_time)
+
+    base_time=`date -d "12am + $base_hours hours $base_seconds seconds $base_minutes minutes"`
+
+    #hours=$(($hours + $locale_offset_hour))
+    diff=`date -d "$base_time $operator $new_hours hours"`
+    diff=`date -d "$diff $operator $new_minutes minutes"`
+    diff=`date -d "$diff $operator $new_seconds seconds"`
+    echo `date -d "$diff"` #+%H:%M`
 }
 
 addTime() {
-    if [[ $# -gt 1 ]]; then
-        time=$2
-    else
-        time="now"
-    fi
-    numFields=$(echo $1 | sed -s 's/[^:]//g' | wc -c | bc)
-    hours=`echo $1 | cut -d ':' -f 1`
-    minutes=0
-    seconds=0
-    if [[ $numFields -gt 1 ]]; then
-        minutes=`echo $1 | cut -d ':' -f 2`
-    fi
-    if [[ $numFields -gt 2 ]]; then
-        seconds=`echo $1 | cut -d ':' -f 3`
-    fi
-    date -d "$time + $hours hours $minutes minutes $seconds seconds" $3
+    _time_add_subtract $@ "+"
 }
 
 subtractTime() {
-    if [[ $# -gt 1 ]]; then
-        time=$2
-    else
-        time="now"
-    fi
-    numFields=$(echo $1 | sed -s 's/[^:]//g' | wc -c | bc)
-    hours=`echo $1 | cut -d ':' -f 1`
-    minutes=0
-    seconds=0
-    if [[ $numFields -gt 1 ]]; then
-        minutes=`echo $1 | cut -d ':' -f 2`
-    fi
-    if [[ $numFields -gt 2 ]]; then
-        seconds=`echo $1 | cut -d ':' -f 3`
-    fi
-    if [[ $# -lt 2 ]]; then
-        date -d "$time - $hours hours $minutes minutes $seconds seconds"
-    else
-        date -d "$time - $hours hours $minutes minutes $seconds seconds" +%H:%M
-    fi
+    _time_add_subtract $@ "-"
 }
 
 diffTimes() {
