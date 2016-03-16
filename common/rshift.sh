4f86069669a80140eecb0efe1aee2b15f3f9ed12
Michael Asnes <mike.asnes@gmail.com>
2016-03-16 11:53:23 -0600

    Revert "march update"
    This reverts commit 5b6660cf5424c0157244ef1d62599c16d5a5f9b9.

--- common/rshift.sh
+++ common/rshift.sh
@@ -7,7 +7,6 @@ args=("$@")
 getopt --test > /dev/null
 if [[ $? != 4 ]]; then
   echo "I’m sorry, `getopt --test` failed in this environment."
-  echo "You must be using the old version of getopt."
   exit 1
 fi
 
@@ -15,9 +14,8 @@ daybrightness=1.0
 nightbrightness=0.9
 
 daytemp=5700
-nighttemp=1500
+nighttemp=2600
 
-rest_of_args=""
 while true; do
   case "$1" in
     -B|--day-brightness)
@@ -40,11 +38,6 @@ while true; do
     -h|--help)
       echo "Usage: (-B|--day-brightness) (-b|--night-brightness) (-T|--day-temp) (-t|--night-temp)"
       exit 0
-      ;;
-    -p|--pass-options)
-      shift
-      rest_of_args=$@
-      break
       ;;
     '')
       break
@@ -69,8 +62,8 @@ fi
 pkill -x redshift
 if [[ numargs -gt 0 ]]; then
   set -x
-  redshift -l 40\.0\:-105\.3 -t $daytemp\:$nighttemp -b $daybrightness:$nightbrightness $rest_of_args -o &
+  redshift -l 40\.0\:-105\.3 -t $daytemp\:$nighttemp -b $daybrightness:$nightbrightness &
 else
   set -x
-   redshift -l 40\.0\:-105\.3 -t $daytemp\:$nighttemp -o &
+   redshift -l 40\.0\:-105\.3 -t $daytemp\:$nighttemp &
 fi
